983cd9fcb4e4121eebd57eda5675b4c18d161da0
Andrea Zanelli <andrea.zanelli@netgloo.com>
2016-01-26 18:09:32 +0100

    Add 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+    <link href="/css/main.css" rel="stylesheet" type="text/css" />
+    <title>Page not found.</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="content">
+        <div class="title">Page not found.</div>
+      </div>
+    </div>
+  </body>
+</html>
